ae05357a366280741749f62834c02f0bd867809e
haribertlondon <bennepalz@yahoo.de>
2019-08-25 14:46:32 +0200

    Better seek process

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ import xbmcgui
 import xbmcplugin
 import json
 import urllib,urllib2
+import time
+import datetime
 
 _url = sys.argv[0] # Get the plugin url in plugin:// notation.
 _handle = int(sys.argv[1]) # Get the plugin handle as an integer number.
 
 def getJSON(url,select):
+    html = ""
     try:
         req = urllib2.Request(url) #run html request
         req.add_header('User-Agent', ' Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
@@ -25,31 +28,51 @@ def getJSON(url,select):
         response.close()
         js = json.loads(html) #convert json -> dic 
         return js['result'][select]
-    except Exception as e:
-        print(e)
+    except Exception as e:        
+        xbmc.log('Syncplayer: Json Error: '+str(e) +' Url='+str(url) + ' Response' + str(html), level=xbmc.LOGNOTICE)
         return {}
 
 def get_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs))
 
-def get_video(ip):
+
+def get_percentage(ip):      
+      
+    for x in range(3): #set 3 retries                    
+        url = 'http://'+ip+'/jsonrpc?request={"jsonrpc":"2.0","method":"Player.GetProperties","params":{"playerid":1,"properties":["percentage"]},"id":"1"}'
+        percentage = getJSON(url,'percentage')
+                
+        if percentage > 0:
+            break
+        else:             
+            xbmc.log('Syncplayer: Position could not be found. Response:' + str(percentage) + 'Retry...',level=xbmc.LOGNOTICE)            
+            
+    return percentage
+
+
+def get_position(percentage, duration):
+    try:
+        position = float(percentage) * float(duration) / 100.0            
+    except:
+        position = 0
+        
+    return position
+        
+
+def get_video(ip):    
     url = 'http://'+ip+'/jsonrpc?request={"jsonrpc": "2.0", "method": "Player.GetItem", "params": { "properties": ["title", "album", "artist", "season", "episode", "duration", "showtitle", "tvshowid", "thumbnail", "file", "fanart", "streamdetails"], "playerid": 1 }, "id": "VideoGetItem"}'
     dic = getJSON(url,'item')
-
-    if not dic or not dic['title']:
-        return {}
     
-    url = 'http://'+ip+'/jsonrpc?request={"jsonrpc":"2.0","method":"Player.GetProperties","params":{"playerid":1,"properties":["percentage"]},"id":"1"}'
-    dic['percentage'] = getJSON(url,'percentage')
-    
-    dic['title'] = ip + ': ' + dic['title']
-    try:
-        dic['position'] = float(dic['percentage']) * float(dic['streamdetails']['video'][0]['duration']) / 100.0
-    except:
-        dic['position'] = 0
-        xbmc.log('Position could not be found.',level=xbmc.LOGNOTICE)
+    if not dic or not dic['title']: # if no movie found, exit
+        dic = {}
+    else:
+        dic['ip'] = ip
+        dic['percentage'] = get_percentage(ip)
+        dic['duration'] = dic['streamdetails']['video'][0]['duration'] 
+        dic['position'] =  get_position(dic['percentage'], dic['duration'])        
+        dic['label'] = ip + ' - ' +time.strftime('%H:%M:%S', time.gmtime(dic['position'])) + " : " + dic['title']
 
-    xbmc.log(str(dic),level=xbmc.LOGNOTICE)
+    xbmc.log('Syncplayer:' + str(dic),level=xbmc.LOGNOTICE)
     return dic 
     
 def get_videos():
@@ -57,10 +80,11 @@ def get_videos():
     videos = []
     
     #last played
-    for ip in ips:
-        dic = get_video(ip)
-        if dic:
-            videos.append(dic)
+    for ip in ips:        
+        if len(ip)>4:
+            dic = get_video(ip)
+            if dic:
+                videos.append(dic)
     return videos
 
 def list_videos():
@@ -80,8 +104,7 @@ def list_videos():
         list_item = xbmcgui.ListItem(label=video['title'])
         # Set additional info for the list item.
         # 'mediatype' is needed for skin to display info for this ListItem correctly.
-        list_item.setInfo('video', {'title': video['title'],
-                                    'mediatype': 'video'})
+        list_item.setInfo('video', {'title': video['label'] , 'mediatype': 'video'})
         
         list_item.setArt({'thumb': video['thumbnail'], 'icon': video['thumbnail'], 'fanart': video['fanart']})
         # Set 'IsPlayable' property to 'true'.
@@ -89,7 +112,7 @@ def list_videos():
         list_item.setProperty('IsPlayable', 'true')
         # Create a URL for a plugin recursive call.
         # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
-        url = get_url(action='play', file=video['file'], position=video['position'])
+        url = get_url(action='play', file=video['file'], position=video['position'], ip=video['ip'], duration=video['duration'])
         # Add the list item to a virtual Kodi folder.
         # is_folder = False means that this item won't open any sub-list.
         is_folder = False
@@ -99,13 +122,38 @@ def list_videos():
     xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     # Finish creating a virtual folder.
     xbmcplugin.endOfDirectory(_handle)
+    
+def set_position(position):
+    for i in range(30*2): # wait 30 seconds        
+        if xbmc.Player().isPlaying():                  
+            for i in range(30*2): # wait 30 seconds                
+                try:
+                    xbmc.Player().seekTime(float(position))
+                except:            
+                    xbmc.log('Syncplayer: Seek did not work',level=xbmc.LOGNOTICE)
+                
+                if abs( float(xbmc.Player().getTime()) - float(position) )<60: #seek has worked +/- 60sec                                     
+                    xbmc.log('Syncplayer: Seek applied successfully',level=xbmc.LOGNOTICE)                    
+                    return
+                else:
+                    time.sleep(0.5)
+        else:
+            time.sleep(0.5)
+            
+    xbmc.log('Syncplayer: Could not set position',level=xbmc.LOGNOTICE)
 
-def play_video(path, position):
-    # Create a playable item with a path to play.
-    play_item = xbmcgui.ListItem(path=path)
-    # Pass the item to the Kodi player.
-    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
-    xbmc.executebuiltin("Seek("+str(position)+")") 
+def play_video(path, position, ip, duration):
+    # Create a playable item with a path to play.    
+    
+    percentage = get_percentage(ip)    
+    newPosition =  get_position(percentage, duration)        
+    maxposition =  max(newPosition, position)    
+    
+    xbmc.log('Syncplayer: maxposition=' + str(maxposition) + 'newpos=' + str(newPosition) + 'pos=' + str(position) + "percent="+ str(percentage) + ' dur=' + str(duration),level=xbmc.LOGNOTICE)
+    xbmc.Player().play(path)
+    
+    set_position(maxposition)
+
 
 def router(paramstring):
     # Parse a URL-encoded paramstring to the dictionary of
@@ -115,7 +163,7 @@ def router(paramstring):
     if params:
         if params['action'] == 'play':
             # Play a video from a provided URL.
-            play_video(params['file'], params['position'])
+            play_video(params['file'], params['position'], params['ip'], params['duration'])
         else:
             raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
     else:
